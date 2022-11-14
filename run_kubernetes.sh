70d4d7e4f48612e36e5a1c98700de3d8ab2a1c1f
kusuma-ss <kusumass.666@gmail.com>
2022-11-14 21:44:36 +0100

    Revert "added readmefile"
    This reverts commit d7fcc52236c536b6d337036f6955d9a20ba0a5fd.
    
    revert to old commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -12,7 +12,7 @@ dockerpath="kusumass/flaskapplication:latest"
 
 kubectl run "flasklearnapplication"  \
     --image="$dockerpath"\
-    --port=80 --labels app="MLapp"
+    --port=80 --labels app="flasklearnapplication"
 
 
 # Step 3:
